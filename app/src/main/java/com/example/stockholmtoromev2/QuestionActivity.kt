5d82abe874ae6f311d15ab8faed92696bb933e5b
Jesper Söderling <jesper.soderling@me.com>
2021-09-17 16:59:04 +0200

    Good Start in projekt

--- app/src/main/java/com/example/stockholmtoromev2/QuestionActivity.kt
+++ app/src/main/java/com/example/stockholmtoromev2/QuestionActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.UserHandle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -14,8 +15,9 @@ class QuestionActivity : AppCompatActivity() {
     lateinit var userSeeQuestionView: TextView
     lateinit var userButton1: Button
     lateinit var userButton2: Button
-    lateinit var UserButton3: Button
-    lateinit var UserButton4: Button
+    lateinit var userButton3: Button
+    lateinit var userButton4: Button
+
 
 
     var diffrentQuestions = QuestionsList()
@@ -25,26 +27,64 @@ class QuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
-git
+
         userSeeFlagView = findViewById(R.id.flaglv)
+        userSeeQuestionView = findViewById(R.id.textView)
+
+        userButton1 = findViewById(R.id.button1)
+            userButton1.setOnClickListener {
+                correctAnswer()
+
+            }
+        userButton2 = findViewById(R.id.button2)
+        userButton3 = findViewById(R.id.button3)
+        userButton4 = findViewById(R.id.button4)
+
+        val q = diffrentQuestions.listOfQuestions[currentQuestionIndex]
+        userSeeFlagView.setImageResource(q.image)
+        userSeeQuestionView.setText(q.questions)
+        userButton1.setText(q.answer[0])
+        userButton2.setText(q.answer[1])
+        userButton3.setText(q.answer[2])
+        userButton4.setText(q.answer[3])
 
 
 
 
-       // currentQuestionIndex ++
 
 
 
-        nextQuestion()
+        // currentQuestionIndex ++
+
+
+
+       //nextQuestion()
+       //correctAnswer()
     }
 
 
 
     fun nextQuestion() {
-     var q = diffrentQuestions.listOfQuestions[currentQuestionIndex]
+     val q = diffrentQuestions.listOfQuestions[currentQuestionIndex]
         userSeeFlagView.setImageResource(q.image)
+        userSeeQuestionView.setText(q.questions)
+        userButton1.setText(q.answer[0])
+        userButton2.setText(q.answer[1])
+        userButton3.setText(q.answer[2])
+        userButton4.setText(q.answer[3])
+        currentQuestionIndex ++
 
     }
+    fun correctAnswer() {
+        val answer = diffrentQuestions.listOfQuestions[currentQuestionIndex]
+        if(answer.correctAnswer == 1) {
+            nextQuestion()
+        }
+
+
+    }
+
+
 
 
 }
