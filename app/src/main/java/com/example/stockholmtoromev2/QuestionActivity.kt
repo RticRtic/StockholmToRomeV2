9519896024a4eee7d1fa981a7823030a35fee437
Jesper Söderling <jesper.soderling@me.com>
2021-09-20 14:18:39 +0200

    new changes

--- app/src/main/java/com/example/stockholmtoromev2/QuestionActivity.kt
+++ app/src/main/java/com/example/stockholmtoromev2/QuestionActivity.kt
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.UserHandle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 
 class QuestionActivity : AppCompatActivity() {
 
@@ -17,72 +15,92 @@ class QuestionActivity : AppCompatActivity() {
     lateinit var userButton2: Button
     lateinit var userButton3: Button
     lateinit var userButton4: Button
-
+    var userPressAnswer: Int = 0
 
 
     var diffrentQuestions = QuestionsList()
+
     var currentQuestionIndex = 0
+    var q: Destination = diffrentQuestions.listOfQuestions[currentQuestionIndex]
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
-
         userSeeFlagView = findViewById(R.id.flaglv)
         userSeeQuestionView = findViewById(R.id.textView)
 
+
         userButton1 = findViewById(R.id.button1)
-            userButton1.setOnClickListener {
-                correctAnswer()
+        userButton1.setOnClickListener {
+            userPressAnswer = 1
+            checkIfCorrectAnswer()
+
+
+        }
+        userButton2 = findViewById(R.id.button2)
+        userButton2.setOnClickListener {
+            userPressAnswer = 2
+           checkIfCorrectAnswer()
+
+        }
+        userButton3 = findViewById(R.id.button3)
+        userButton3.setOnClickListener {
+            userPressAnswer = 3
+            checkIfCorrectAnswer()
+
+
+        }
+        userButton4 = findViewById(R.id.button4)
+        userButton4.setOnClickListener {
+            userPressAnswer = 4
+           checkIfCorrectAnswer()
+
+        }
+
+
+        setQuestion()
+
+    }
+
+
+    fun setQuestion() {
+
+
+
+        q = diffrentQuestions.listOfQuestions[currentQuestionIndex]
+        userSeeFlagView.setImageResource(q.image)
+        userSeeQuestionView.setText(q.question)
+        userButton1.setText(q.answer[0])
+        userButton2.setText(q.answer[1])
+        userButton3.setText(q.answer[2])
+        userButton4.setText(q.answer[3])
+
+
+    }
+
+    fun checkIfCorrectAnswer() {
+        if (q.correctAnswer == userPressAnswer) {
+            currentQuestionIndex++
+
+            when {
+                currentQuestionIndex <= diffrentQuestions.listOfQuestions.size ->
+                    setQuestion()
+            }
+
+
+        } else {
+            val question = diffrentQuestions.listOfQuestions.get(currentQuestionIndex)
+            if(question.correctAnswer != userPressAnswer) {
+                userSeeQuestionView.setText("Hej")
+
 
             }
-        userButton2 = findViewById(R.id.button2)
-        userButton3 = findViewById(R.id.button3)
-        userButton4 = findViewById(R.id.button4)
-
-        val q = diffrentQuestions.listOfQuestions[currentQuestionIndex]
-        userSeeFlagView.setImageResource(q.image)
-        userSeeQuestionView.setText(q.questions)
-        userButton1.setText(q.answer[0])
-        userButton2.setText(q.answer[1])
-        userButton3.setText(q.answer[2])
-        userButton4.setText(q.answer[3])
-
-
-
-
-
-
-
-        // currentQuestionIndex ++
-
-
-
-       //nextQuestion()
-       //correctAnswer()
-    }
-
-
-
-    fun nextQuestion() {
-     val q = diffrentQuestions.listOfQuestions[currentQuestionIndex]
-        userSeeFlagView.setImageResource(q.image)
-        userSeeQuestionView.setText(q.questions)
-        userButton1.setText(q.answer[0])
-        userButton2.setText(q.answer[1])
-        userButton3.setText(q.answer[2])
-        userButton4.setText(q.answer[3])
-        currentQuestionIndex ++
-
-    }
-    fun correctAnswer() {
-        val answer = diffrentQuestions.listOfQuestions[currentQuestionIndex]
-        if(answer.correctAnswer == 1) {
-            nextQuestion()
         }
 
 
     }
+
 
 
 
