c343b675034f7bebc60e856d75b8f92c28155be1
mansah82 <manne.sahlin@iths.se>
2021-09-20 15:56:01 +0200

    added nothing

--- app/src/main/java/com/example/stockholmtoromev2/QuestionActivity.kt
+++ app/src/main/java/com/example/stockholmtoromev2/QuestionActivity.kt
@@ -24,6 +24,7 @@ class QuestionActivity : AppCompatActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
